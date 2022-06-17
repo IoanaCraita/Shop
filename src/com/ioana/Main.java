cc9a4ab846e5ae804e1cb77a08c18716c776c349
test <test@gmail.com>
2022-06-17 21:35:34 +0300

    final shop

--- src/com/ioana/Main.java
+++ src/com/ioana/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Service service = new Service();
-        service.method();
+        service.placeOrder();
     }
 }
--- src/com/ioana/Service.java
+++ src/com/ioana/Service.java
@@ -1,8 +1,10 @@
 package com.ioana;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,45 +12,62 @@ import java.util.Map;
 
 public class Service {
 
-    public void method(){
+//    private List<Product> generateProducts(){
+//        List<Product> catalog = new ArrayList<>();
+//        Product purcari = new Wine("Purcari", 35.0, 100, 10);
+//        Product chocolate = new Cake("Chocolate Cake", 75.5, 2, 1500);
+//        Product cabernet = new Wine("Cabernet", 56.0, 36, 3);
+//        Product tiramisu = new Cake("Tiramisu", 12.0, 10, 400);
+//        catalog.add(purcari);
+//        catalog.add(chocolate);
+//        catalog.add(cabernet);
+//        catalog.add(tiramisu);
+//
+//        return catalog;
+//    }
 
+    public Map<Product, Integer> readOrder() {
+      //  List<Product> catalog = generateProducts();
         Cart cart = new Cart();
 
         List<Product> catalog = new ArrayList<>();
-        Product purcari = new Wine("Purcari",35.0,100,10);
-        Product chocolate = new Cake("Chocolate Cake",75.5,2,1500);
-        Product cabernet = new Wine("Cabernet",56.0,36,3);
+        Product purcari = new Wine("Purcari", 35.0, 100, 10);
+        Product chocolate = new Cake("Chocolate Cake", 75.5, 2, 1500);
+        Product cabernet = new Wine("Cabernet", 56.0, 36, 3);
         Product tiramisu = new Cake("Tiramisu", 12.0, 10, 400);
         catalog.add(purcari);
         catalog.add(chocolate);
         catalog.add(cabernet);
         catalog.add(tiramisu);
 
-        for (Product product :catalog){
-            System.out.println( (catalog.indexOf(product)+1) + "." +  product.getName() +" " + product.getPrice() + " RON");
+        for (Product product : catalog) {
+            System.out.println((catalog.indexOf(product) + 1) + "." + product.getName() + " " + product.getPrice() + " RON");
         }
-        Map<Product,Integer> productsInCart = new HashMap<>();
+        Map<Product, Integer> productsInCart = new HashMap<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         boolean addMore = true;
-        while (addMore){
+        while (addMore) {
             System.out.println("Select product number + quantity");
             try {
-               // String order = reader.readLine();
+                // String order = reader.readLine();
                 String[] orderEntry = reader.readLine().split(" ");
                 int productNumber = Integer.parseInt(orderEntry[0]);
                 Integer quantity = Integer.parseInt(orderEntry[1]);
-                switch (productNumber){
+
+
+                switch (productNumber) {
                     case 1:
-                        productsInCart.put(purcari,quantity);
+                        productsInCart.put(purcari, productsInCart.getOrDefault(purcari, 0) + quantity);
                         break;
                     case 2:
-                        productsInCart.put(chocolate,quantity);
+                        productsInCart.put(chocolate, productsInCart.getOrDefault(purcari, 0) + quantity);
                         break;
                     case 3:
-                        productsInCart.put(cabernet,quantity);
+                        productsInCart.put(cabernet, productsInCart.getOrDefault(purcari, 0) + quantity);
                         break;
                     case 4:
-                        productsInCart.put(tiramisu,quantity);
+                        productsInCart.put(tiramisu, productsInCart.getOrDefault(purcari, 0) + quantity);
+                        break;
                     default:
                         System.out.println("Invalid option");
                 }
@@ -56,14 +75,53 @@ public class Service {
                 System.out.println("blabla");
                 ioException.printStackTrace();
             }
-
+            System.out.println("Do you still want to buy? yes/no");
+            String addMoreString = null;
+            try {
+                addMoreString = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (addMoreString != null) {
+                addMore = addMoreString.equals("yes");
+            }
+            cart.setProducts(productsInCart);
         }
+
+        return cart.getProducts();
     }
 
+    public int calcTotalQuantity(Map<Product, Integer> products) {
+        int totalQty = 0;
+        for (int i : products.values()) {
+            totalQty = totalQty + i;
+        }
+        return totalQty;
+    }
 
+    private double calcCartTotal(Map<Product, Integer> products) {
+        double total = 0;
+        for (Product product : products.keySet()) {
+            total = total + (product.getPrice() * products.get(product));
+        }
+        return total;
+    }
 
+    public void placeOrder() {
+        Map<Product, Integer> productsInCart = readOrder();
+        System.out.println(" ");
+        System.out.println("Your order was successful! Thank you for shopping with us.");
+        System.out.println("---------Order Summary ---------- ");
+        System.out.println("Order Date: " + LocalDate.now());
+        System.out.println(" ");
+        for (Product product : productsInCart.keySet()) {
+            System.out.println(product.getName() + "   Qty:  " + productsInCart.get(product) + "   Price:  " +  product.getPrice() + " RON");
+        }
+        System.out.println("-------------------------------------");
+        System.out.println("Total Quantity: " + calcTotalQuantity(productsInCart));
+        System.out.println("Order Total " + calcCartTotal(productsInCart) + " RON");
 
-
+    }
 
 
 // SA APARA UN CATALOG DE PRODUSE
